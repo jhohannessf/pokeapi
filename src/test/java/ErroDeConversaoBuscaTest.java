d4c04d5cc42ac6f47914d326b1a1f39fa826d184
jhohannessf <jhohannes.sf@gmail.com>
2026-05-28 18:42:32 -0300

    update na classe de ErroDeConversaoBuscaTest

--- src/test/java/ErroDeConversaoBuscaTest.java
+++ src/test/java/ErroDeConversaoBuscaTest.java
@@ -1,3 +1,4 @@
+import co.pokeapi.excecao.ErroDeConversaoBusca;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,14 +11,10 @@ public class ErroDeConversaoBuscaTest {
     @Test
     void DeveTerMensagem(){
         String mensagem = "Não consegui capturar este Pokemon na busca (Nome ou ID.Dex inválido).";
-        ErroDeConversaoBuscaTest erro = new ErroDeConversaoBuscaTest(mensagem);
+        ErroDeConversaoBusca erro = new ErroDeConversaoBusca(mensagem);
 
         //Verifica se a mensagem está certa
         assertEquals(mensagem, erro.getMessage());
 
-    }
-
-    private String getMessage() {
-        return getMessage();
     }
 }
