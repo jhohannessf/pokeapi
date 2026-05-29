8ccc943f9c0cf33c6e440554122081df6153949c
jhohannessf <jhohannes.sf@gmail.com>
2026-05-29 19:38:45 -0300

    Metodo novo listarCapturados, que trata a  Manipulação de arquivos — leitura e escrita de arquivos JSON no disco

--- src/main/java/co/pokeapi/modelos/GerenciadorDeArquivos.java
+++ src/main/java/co/pokeapi/modelos/GerenciadorDeArquivos.java
@@ -7,6 +7,9 @@ import com.google.gson.GsonBuilder;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GerenciadorDeArquivos {
     private String arquivo;
@@ -38,6 +41,44 @@ public class GerenciadorDeArquivos {
         }
     }
 
+    public List<Pokemon> listarCapturados() throws IOException {
+        //Cria uma lista Vazia dos Pokemons encontrados
+        List<Pokemon> capturados = new ArrayList<>();
+
+        //Cria um objeto File apontando para a pasta pokemons/
+        //Não cria a pasta, só cria uma referência para ela, como um endereço.
+        File pasta = new File("pokemons");
+
+        //Verifica se a pasta existe e tem arquivos
+        if (!pasta.exists() || pasta.listFiles() == null) {
+            System.out.println("Nenhum Pokemon capturado ainda.");
+            return capturados;
+        }
+
+        //Cria o Gson com mesmo FieldNamingPolicy usado ao salvar
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .create();
+
+        // Percorre cada arquivo .json da pasta
+        // O pasta.listFiles() retorna um array com todos os arquivos da pasta.
+        for (File arquivo : pasta.listFiles()){ // Lê assim: "para cada File dentro de pasta.listFiles(), chame de arquivo"
+            //Filtra apenas arquivos .json, ignorando outros tipos
+            if (arquivo.getName().endsWith(".json")){
+                // Lê o conteúdo do arquivo e converte para uma String
+                String conteudo = new String(Files.readAllBytes(arquivo.toPath()));
+
+                //Converte a String JSON de volta para um objeto Pokemon
+                Pokemon pokemon = gson.fromJson(conteudo, Pokemon.class);
+
+                //Adiciona o Pokemon convertido na lista.
+                capturados.add(pokemon);
+            }
+        }
+        //Retorna a lista completa com todos os Pokemons encontrados na pasta
+        return capturados;
 }
+}
+
 
 
